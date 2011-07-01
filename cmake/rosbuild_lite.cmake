6bd47ded1d31cace8833a3f3ac2681617f732bc4
Ethan Rublee <ethan.rublee@gmail.com>
2011-07-01 16:45:49 -0700

    ros lite.

---
+++ cmake/rosbuild_lite.cmake
@@ -0,0 +1,24 @@
+macro (rospack VAR)
+  if(NOT ${VAR}_CACHED)
+    execute_process(COMMAND rospack ${ARGN}
+      OUTPUT_VARIABLE ${VAR}
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+      )
+    separate_arguments(${VAR} UNIX_COMMAND ${${VAR}})
+    set(${VAR} ${${VAR}} CACHE STRING "${VAR} value")
+    set(${VAR}_CACHED TRUE CACHE BOOL "${VAR} cached flag")
+    message(STATUS "${VAR} := ${${VAR}}")
+  else()
+    message(STATUS "BANG! Using cached ${VAR}")
+  endif()
+endmacro()
+
+macro (find_ros_package NAME)
+  rospack(${NAME}_INCLUDES cflags-only-I ${NAME})
+  include_directories(${${NAME}_INCLUDES})
+  rospack(${NAME}_DEFINITIONS cflags-only-other ${NAME})
+  add_definitions(" ${${NAME}_DEFINITIONS}")
+  rospack(${NAME}_LIBRARY_DIRS libs-only-L ${NAME})
+  rospack(${NAME}_LIBRARIES libs-only-l ${NAME})
+  link_directories(${${NAME}_LIBRARY_DIRS})
+endmacro()
