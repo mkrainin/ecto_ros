7398038df952f46fc10b62b6abec774667c89324
Ethan Rublee <ethan.rublee@gmail.com>
2011-07-04 19:18:46 -0700

    Remove windows line endings. Version str protect.
    deps to reflect what is actually needed.
    
    Adding argv stripping to init function.
    
    redentation.
    
    dentation.
    
    Will strip.
    
    Stripping options.

--- src/ecto_ros.cpp
+++ src/ecto_ros.cpp
@@ -9,58 +9,76 @@
 #include <iostream>
 #include <string>
 
-struct Spinner
+namespace
 {
-  void
-  operator()()
+  struct Spinner
   {
-    std::cout << "Spinning up."<< std::endl;
-    ros::spin();
-    std::cout << "Spinning down." << std::endl;
-  }
-};
-
-struct RosLifetime
-{
-  RosLifetime():thread_(new boost::thread(Spinner()))
-  {
-  }
-  ~RosLifetime()
-  {
-    if(ros::ok())
+    void
+    operator()()
     {
-      std::cout << "Shutting node down."<< std::endl;
-      ros::requestShutdown();
+//      std::cout << "Spinning up." << std::endl;
+      ros::spin();
+//      std::cout << "Spinning down." << std::endl;
     }
-    std::cout << "Joining ros thread." << std::endl;
-    thread_->join();
-    std::cout << "Goodnight." << std::endl;
-  }
-  boost::scoped_ptr<boost::thread> thread_;
-  static boost::scoped_ptr<RosLifetime> its_a_ros_life_;
-};
-boost::scoped_ptr<RosLifetime>  RosLifetime::its_a_ros_life_;
+  };
 
-namespace bp = boost::python;
-void
-ros_init_wtf(bp::object sys_argv, const std::string& node_name)
-{
-  std::vector<std::string> args;
-  bp::stl_input_iterator<std::string> begin(sys_argv), end;
-  std::copy(begin, end, std::back_inserter(args));
-  char ** argv = new char*[args.size()];
-  for (int i = 0, ie = args.size(); i < ie; ++i)
+  struct RosLifetime
   {
-    argv[i] = const_cast<char*> (args[i].data());
-  }
-  int ac = args.size();
-  ros::init(ac, argv, node_name.c_str(), ros::init_options::NoSigintHandler|ros::init_options::AnonymousName);
-  delete[] argv;
+    RosLifetime()
+        :
+          thread_(new boost::thread(Spinner()))
+    {
+    }
+    ~RosLifetime()
+    {
+      if (ros::ok())
+      {
+//        std::cout << "Shutting node down." << std::endl;
+        ros::requestShutdown();
+      }
+//      std::cout << "Joining ros thread." << std::endl;
+      thread_->join();
+//      std::cout << "Goodnight." << std::endl;
+    }
+    boost::scoped_ptr<boost::thread> thread_;
+    static boost::scoped_ptr<RosLifetime> its_a_ros_life_;
+  };
 
-  RosLifetime::its_a_ros_life_.reset(new RosLifetime());
+  boost::scoped_ptr<RosLifetime> RosLifetime::its_a_ros_life_;
+  namespace bp = boost::python;
+
+  void
+  ros_init(bp::list sys_argv, const std::string& node_name)
+  {
+    std::vector<std::string> args;
+    bp::stl_input_iterator<std::string> begin(sys_argv), end;
+    std::copy(begin, end, std::back_inserter(args));
+    char** argv = new char*[args.size()]; //array to emulate argv.
+    for (int i = 0, ie = args.size(); i < ie; ++i)
+    {
+      argv[i] = const_cast<char*>(args[i].data());
+    }
+
+    int ac = args.size();
+    ros::init(ac, argv, node_name.c_str(), ros::init_options::NoSigintHandler | ros::init_options::AnonymousName);
+
+    //forward the arg stripping back to python
+    while (bp::len(sys_argv))
+      sys_argv.pop();
+    for (int i = 0; i < ac; ++i)
+    {
+      sys_argv.append(bp::str((const char*) (argv[i])));
+    }
+    delete[] argv;
+    RosLifetime::its_a_ros_life_.reset(new RosLifetime());
+  }
+
 }
 
 ECTO_DEFINE_MODULE(ecto_ros)
 {
-  bp::def("init",ros_init_wtf, "Calls roscpp initialization routine. Please call with sys.argv, or similar list of commandline args. Will not strip them...");
+  bp::def(
+      "init",
+      ros_init,
+      "Calls roscpp initialization routine. Please call with sys.argv, or similar list of commandline args. Will strip them...");
 }
--- src/usb_camera.py
+++ src/usb_camera.py
@@ -5,11 +5,12 @@ import ecto
 import ecto_ros, ecto_sensor_msgs
 from ecto_opencv import highgui
 import sys
+import argparse
 
-def do_ecto(device_id=0,frame_id='base'):
+def do_ecto(device_id=0,frame_id='base', threads=1):
     video_capture = highgui.VideoCapture('Video Camera',video_device=device_id)
     mat2image = ecto_ros.Mat2Image(frame_id=frame_id,encoding='bgr8')
-    pub_rgb = ecto_sensor_msgs.Publisher_Image("image pub", topic_name='cvcamera/image')
+    pub_rgb = ecto_sensor_msgs.Publisher_Image("image pub", topic_name='image')
     graph = [
                 video_capture["image"] >> mat2image["image"],
                 mat2image["image"] >> pub_rgb["input"]
@@ -17,12 +18,22 @@ def do_ecto(device_id=0,frame_id='base'):
     
     plasm = ecto.Plasm()
     plasm.connect(graph)
-    ecto.view_plasm(plasm)
-    sched = ecto.schedulers.Threadpool(plasm)
-    try:
-        sched.execute(8)
-    except RuntimeError, e:
-        print e
+    if threads == 1:
+        sched = ecto.schedulers.Singlethreaded(plasm)
+        sched.execute()
+    else:
+        sched = ecto.schedulers.Threadpool(plasm)
+        sched.execute(threads)
 if __name__ == "__main__":
     ecto_ros.init(sys.argv, "image_pub")
-    do_ecto()
+    
+    parser = argparse.ArgumentParser(description='Turns a /dev/video* into a ros image publisher.')
+    parser.add_argument('--device_id', metavar='N',  dest='device_id', type=int,default=0,
+                       help='the device id to open.')
+    parser.add_argument('--frame_id', dest='frame_id', type=str, default='base',
+                       help='The frame id to associate this camera with.')
+    parser.add_argument('--threads', dest='threads', type=int, default=1,
+                       help='The number of threads to run with.')
+    args = parser.parse_args()
+    
+    do_ecto(args.device_id,args.frame_id,args.threads)
